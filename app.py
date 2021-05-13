85c3b87418c050091dc9b4d740c664316f012f06
shobhit977 <shobhit00gupta@gmail.com>
2021-05-13 13:46:06 +0530

    third commit

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/view_details')
 def details():
     return render_template("view_details.html")
+
 
 @app.route('/upload')
 def upload():
@@ -17,6 +20,6 @@ def upload():
 def send_mail():
     return render_template("send_mail.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
